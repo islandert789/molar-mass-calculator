43e2ab58b9617c9c7aa432548a50a03e55de9c34
islandert789 <islandert789@yahoo.com>
2016-11-12 15:06:16 -0800

    Added support for parenthases (but use brackets) For some reason bash does not like using parenthases in command line arguments.

--- molarMass.cpp
+++ molarMass.cpp
@@ -2,11 +2,7 @@
 #include<map>
 #include<string>
 using namespace std;
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		cout << "Usage error: please use one command line argument.\n";
-		return 1;
-	}
+void incrMass(int multiplier, string elementName, string elementN, double & mass) {
 	map<string, double> massMap;
 	massMap["H"] = 1.008;
 	massMap["He"] = 4.00;
@@ -46,21 +42,78 @@ int main(int argc, char *argv[]) {
 	massMap["Br"] = 79.90;
 	massMap["Kr"] = 83.80;
 	massMap["Uut"] = 284;
-
+	if (elementN != "") {
+		multiplier *= stoi(elementN);
+	}
+	cout << "\tIncreasing mass by " << multiplier << " " << elementName << endl;
+	mass += massMap.at(elementName) * multiplier;
+}
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		cout << "Usage error: please use one command line argument.\n";
+		return 1;
+	}
 	string molecule = argv[1];
 	string elementName = "";
 	string elementN = "";
+	int multiplier = 1;
 	double mass = 0;
 	for (int i = 0; i < molecule.length(); i++) {
-		if (isupper(molecule[i])) {
+		cout << "Parsing symbol " << molecule[i] << endl;
+		if (molecule[i] == '[') {
 			if (elementName != "") {
-				if (elementN == "") {
-					mass += massMap.at(elementName);
-					//cout << "Increasing mass by 1 " << elementName << endl;
-				} else {
-					mass += massMap.at(elementName)*stoi(elementN);
-					//cout << "Increasing mass by " << stoi(elementN) << " " << elementName << endl;
+				incrMass(multiplier, elementName, elementN, mass);
+				elementName = "";
+				elementN = "";
+			}
+			cout << "\tFound a [, starting loop to find matching ]\n";
+			int parenN = 1;
+			for (int j = i+1; j < molecule.length(); j++) {
+				cout << "\t\tlooking at " << molecule[j] << endl;
+				if (molecule[j] == '[') {
+					parenN++;
+				} else if (molecule[j] == ']') {
+					parenN--;
 				}
+				if (parenN == 0) {
+					cout << "\t\t found matching ], looping to get multiplier\n";
+					string m = "";
+					int k = j+1;
+					while (isdigit(molecule[k]) && k < molecule.length()) {
+						cout << "\t\t\tLooking at " << molecule[k] << endl;
+						m += molecule[k];
+						k++;
+					}
+					if (m != "") {
+						cout << "\t\t\tScaling multiplier by " << stoi(m) << endl;
+						multiplier *= stoi(m);
+					}
+					break;
+				}
+			}
+		} else if (molecule[i] == ']') {
+			if (elementName != "") {
+				incrMass(multiplier, elementName, elementN, mass);
+				elementName = "";
+				elementN = "";
+			}
+			cout << "\tFound ], looking for following number\n";
+			string m = "";
+			int j = i+1;
+			while (isdigit(molecule[j]) && j < molecule.length()) {
+				cout << "\t\tLooking at " << molecule[j] << endl;
+				m += molecule[j];
+				j++;
+			}
+			cout << "\tSkipping ahead " << j - i - 1 << " numbers\n";
+			i += j - i - 1;
+			if (m != "") {
+				cout << "\tDecreasing multiplier by " << stoi(m) << endl;
+				multiplier /= stoi(m);
+			}
+		} else if (isupper(molecule[i])) {
+			if (elementName != "") {
+				incrMass(multiplier, elementName, elementN, mass);
 				elementName = molecule[i];
 				elementN = "";
 			} else {
@@ -79,12 +132,8 @@ int main(int argc, char *argv[]) {
 			//cout << "Set the element number of the element " << elementName << " to " << elementN << endl;
 		}
 	}
-	if (elementN == "") {
-		mass += massMap.at(elementName);
-		//cout << "Increasing mass by 1 " << elementName << endl;
-	} else {
-		mass += massMap.at(elementName)*stoi(elementN);
-		//cout << "Increasing mass by " << stoi(elementN) << " " << elementName << endl;
+	if (elementName != "") {
+		incrMass(multiplier, elementName, elementN, mass);
 	}
 	cout << "Molar Mass: " << mass << endl;
 	return 0;
