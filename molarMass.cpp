32d8c15b8d55c93d031ecef30566fbbd36105ab5
islandert789 <islandert789@yahoo.com>
2016-11-12 15:09:48 -0800

    Cleaned up the program. - commented out cout debugging statements - moved the incrMass function definition to end of program

--- molarMass.cpp
+++ molarMass.cpp
@@ -2,6 +2,99 @@
 #include<map>
 #include<string>
 using namespace std;
+void incrMass(int multiplier, string elementName, string elementN, double & mass);
+//Increments the mass using the pareneters
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		//cout << "Usage error: please use one command line argument.\n";
+		return 1;
+	}
+	string molecule = argv[1];
+	string elementName = "";
+	string elementN = "";
+	int multiplier = 1;
+	double mass = 0;
+	for (int i = 0; i < molecule.length(); i++) {
+		//cout << "Parsing symbol " << molecule[i] << endl;
+		if (molecule[i] == '[') {
+			if (elementName != "") {
+				incrMass(multiplier, elementName, elementN, mass);
+				elementName = "";
+				elementN = "";
+			}
+			//cout << "\tFound a [, starting loop to find matching ]\n";
+			int parenN = 1;
+			for (int j = i+1; j < molecule.length(); j++) {
+				//cout << "\t\tlooking at " << molecule[j] << endl;
+				if (molecule[j] == '[') {
+					parenN++;
+				} else if (molecule[j] == ']') {
+					parenN--;
+				}
+				if (parenN == 0) {
+					//cout << "\t\t found matching ], looping to get multiplier\n";
+					string m = "";
+					int k = j+1;
+					while (isdigit(molecule[k]) && k < molecule.length()) {
+						//cout << "\t\t\tLooking at " << molecule[k] << endl;
+						m += molecule[k];
+						k++;
+					}
+					if (m != "") {
+						//cout << "\t\t\tScaling multiplier by " << stoi(m) << endl;
+						multiplier *= stoi(m);
+					}
+					break;
+				}
+			}
+		} else if (molecule[i] == ']') {
+			if (elementName != "") {
+				incrMass(multiplier, elementName, elementN, mass);
+				elementName = "";
+				elementN = "";
+			}
+			//cout << "\tFound ], looking for following number\n";
+			string m = "";
+			int j = i+1;
+			while (isdigit(molecule[j]) && j < molecule.length()) {
+				//cout << "\t\tLooking at " << molecule[j] << endl;
+				m += molecule[j];
+				j++;
+			}
+			//cout << "\tSkipping ahead " << j - i - 1 << " numbers\n";
+			i += j - i - 1;
+			if (m != "") {
+				//cout << "\tDecreasing multiplier by " << stoi(m) << endl;
+				multiplier /= stoi(m);
+			}
+		} else if (isupper(molecule[i])) {
+			if (elementName != "") {
+				incrMass(multiplier, elementName, elementN, mass);
+				elementName = molecule[i];
+				elementN = "";
+			} else {
+				elementName = molecule[i];
+				//cout << "Found the first character of the element name: " << elementName << endl;
+			}
+		} else if (islower(molecule[i])) {
+			elementName += molecule[i];
+			//cout << "Added a lowercase letter to the element name: " << elementName << endl;
+		} else if (isdigit(molecule[i])) {
+			if (elementName.length() == 0) {
+				//cout << "There needs to be something for a number to follow\n";
+				return 1;
+			}
+			elementN += molecule[i];
+			//cout << "Set the element number of the element " << elementName << " to " << elementN << endl;
+		}
+	}
+	if (elementName != "") {
+		incrMass(multiplier, elementName, elementN, mass);
+	}
+	cout << "Molar Mass: " << mass << endl;
+	return 0;
+}
 void incrMass(int multiplier, string elementName, string elementN, double & mass) {
 	map<string, double> massMap;
 	massMap["H"] = 1.008;
@@ -47,94 +140,4 @@ void incrMass(int multiplier, string elementName, string elementN, double & mass
 	}
 	cout << "\tIncreasing mass by " << multiplier << " " << elementName << endl;
 	mass += massMap.at(elementName) * multiplier;
-}
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		cout << "Usage error: please use one command line argument.\n";
-		return 1;
-	}
-	string molecule = argv[1];
-	string elementName = "";
-	string elementN = "";
-	int multiplier = 1;
-	double mass = 0;
-	for (int i = 0; i < molecule.length(); i++) {
-		cout << "Parsing symbol " << molecule[i] << endl;
-		if (molecule[i] == '[') {
-			if (elementName != "") {
-				incrMass(multiplier, elementName, elementN, mass);
-				elementName = "";
-				elementN = "";
-			}
-			cout << "\tFound a [, starting loop to find matching ]\n";
-			int parenN = 1;
-			for (int j = i+1; j < molecule.length(); j++) {
-				cout << "\t\tlooking at " << molecule[j] << endl;
-				if (molecule[j] == '[') {
-					parenN++;
-				} else if (molecule[j] == ']') {
-					parenN--;
-				}
-				if (parenN == 0) {
-					cout << "\t\t found matching ], looping to get multiplier\n";
-					string m = "";
-					int k = j+1;
-					while (isdigit(molecule[k]) && k < molecule.length()) {
-						cout << "\t\t\tLooking at " << molecule[k] << endl;
-						m += molecule[k];
-						k++;
-					}
-					if (m != "") {
-						cout << "\t\t\tScaling multiplier by " << stoi(m) << endl;
-						multiplier *= stoi(m);
-					}
-					break;
-				}
-			}
-		} else if (molecule[i] == ']') {
-			if (elementName != "") {
-				incrMass(multiplier, elementName, elementN, mass);
-				elementName = "";
-				elementN = "";
-			}
-			cout << "\tFound ], looking for following number\n";
-			string m = "";
-			int j = i+1;
-			while (isdigit(molecule[j]) && j < molecule.length()) {
-				cout << "\t\tLooking at " << molecule[j] << endl;
-				m += molecule[j];
-				j++;
-			}
-			cout << "\tSkipping ahead " << j - i - 1 << " numbers\n";
-			i += j - i - 1;
-			if (m != "") {
-				cout << "\tDecreasing multiplier by " << stoi(m) << endl;
-				multiplier /= stoi(m);
-			}
-		} else if (isupper(molecule[i])) {
-			if (elementName != "") {
-				incrMass(multiplier, elementName, elementN, mass);
-				elementName = molecule[i];
-				elementN = "";
-			} else {
-				elementName = molecule[i];
-				//cout << "Found the first character of the element name: " << elementName << endl;
-			}
-		} else if (islower(molecule[i])) {
-			elementName += molecule[i];
-			//cout << "Added a lowercase letter to the element name: " << elementName << endl;
-		} else if (isdigit(molecule[i])) {
-			if (elementName.length() == 0) {
-				//cout << "There needs to be something for a number to follow\n";
-				return 1;
-			}
-			elementN += molecule[i];
-			//cout << "Set the element number of the element " << elementName << " to " << elementN << endl;
-		}
-	}
-	if (elementName != "") {
-		incrMass(multiplier, elementName, elementN, mass);
-	}
-	cout << "Molar Mass: " << mass << endl;
-	return 0;
 }
